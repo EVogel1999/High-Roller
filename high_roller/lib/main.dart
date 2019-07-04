4b01692ec4ec4aea66a95a3ccc2145be6de8bad2
EVogel1999 <eav990@gmail.com>
2019-07-03 20:54:59 -0400

    Create number of dice to roll buttons

--- high_roller/lib/main.dart
+++ high_roller/lib/main.dart
@@ -1,5 +1,5 @@
-//import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flare_flutter/flare_actor.dart';
 
 main() => runApp(App());
 
@@ -12,6 +12,7 @@ class App extends StatefulWidget {
 
 class _AppState extends State<App> {
   List<String> _dice = ['D20', 'D12', 'D10', 'D8', 'D6', 'D4'];
+  int _diceRolled = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -30,11 +31,9 @@ class _AppState extends State<App> {
         ),
         body: Column(children: [
           Container(
+              alignment: Alignment.topCenter,
               margin: EdgeInsets.all(10.0),
-              child: RaisedButton(
-                  onPressed: () {},
-                  child: Text('Add Product')
-              )
+              child: _getButtons()
           ),
           Flexible(child: ListView(
               padding: const EdgeInsets.all(8.0),
@@ -48,16 +47,54 @@ class _AppState extends State<App> {
     return ThemeData(
         brightness: Brightness.light,
         primaryColor: Colors.red,
-        accentColor: Colors.red
+        accentColor: Colors.redAccent
     );
   }
 
+  /// Creates and maintains the state of the number of dice to roll buttons
+  Widget _getButtons() {
+    return Container(
+        margin: EdgeInsets.only(top: 10.0, bottom: 10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              color: Colors.red,
+              iconSize: 40.0,
+              icon: Icon(Icons.remove),
+              onPressed: () {
+                setState(() {
+                  if (_diceRolled > 1) {
+                    _diceRolled = _diceRolled - 1;
+                  }
+                });
+              },
+            ),
+          Container(
+              margin: EdgeInsets.only(left: 30.0, right: 30.0),
+              child: Text(_diceRolled.toString(), style: TextStyle(fontSize: 24.0))
+          ),
+          IconButton(
+            color: Colors.green,
+            iconSize: 40.0,
+            icon: Icon(Icons.add),
+            onPressed: () {
+              setState(() {
+                _diceRolled = _diceRolled + 1;
+              });
+            }
+          )
+        ]
+      )
+    );
+  }
+
+  /// Creates the dice buttons
   List<Widget> _getDice() {
     List<Widget> result = [];
     _dice.forEach((element) {
       result.add(Card(child: Column(
         children: [
-          Image.asset('assets/icon.png'),
           Text(element)
         ],
       )));
